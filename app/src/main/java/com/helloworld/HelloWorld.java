527f173b61f9ea136cf6c7c7629d1fbaaf0a15c0
geezann <brayen.chanzezar@si.ukdw.ac.id>
2019-08-23 19:18:30 +0700

    Initial commit

---
+++ app/src/main/java/com/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.helloworld;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstaceState){
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.hello_world_activity);
+    }
+}
